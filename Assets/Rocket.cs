6d515bea7405d8652ef54106c6d94617cd3ba6bb
Gabriel <gab.francez@gmail.com>
2020-04-18 13:10:25 +0200

    Thrusting rocket input

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            print("Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotate Right");
+        }
+    }
+}
